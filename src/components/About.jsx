9c5080a65b42c5efa01df0eb7ea9ca68dc935348
Javier Mombiela <61723252+javim7@users.noreply.github.com>
2023-10-25 21:18:42 -0600

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
 
-const About = () => {
+import { styles } from '../styles'
+import { services } from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon}) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div 
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+      </motion.div>
+
+      <motion.p 
+      variants={fadeIn("", "", 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] leading-[30px]'
+      >
+        I'm a computer science student from Guatemala currently in my 4th year of undergraduate studies. Through my coursework, I've gained expertise in a variety of programming languages, and skills.
+        I am a lifelong learner, and I enjoy exploring new skills and programming languages. For me, every day is an opportunity to learn something new and exciting in the tech world. I am always on the lookout for new challenges and projects that can help me grow my skills and experience.
+        Overall, I'm a dedicated and enthusiastic computer science student with a passion for learning and a drive to succeed. I'm excited to continue growing my skills and experience as I progress through my studies and pursue my career in the tech field
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
